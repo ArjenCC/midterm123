956f7abb674152520361b0a0e8759d2e5341c4af
ArjenCC <113308394+ArjenCC@users.noreply.github.com>
2023-03-03 15:02:10 -0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI":"mongodb+srv://Arjen:6D9lukYFyINH868e@mongodbserver.vuncwmt.mongodb.net/contact?retryWrites=true&w=majority"
+  "URI":"mongodb+srv://Arjen:6D9lukYFyINH868e@mongodbserver.vuncwmt.mongodb.net/book_store?retryWrites=true&w=majority"
 };
